01e28f89e9792332b45fc7959014e724a3e72d35
hekailiang <hekailiang@gmail.com>
2013-06-19 23:57:40 +0800

    Include js dependencies in addon, no need to extend servlet anymore

--- nanoscrollpanel/src/main/java/org/vaadin/hhe/nanoscrollpanel/NanoScrollPanel.java
+++ nanoscrollpanel/src/main/java/org/vaadin/hhe/nanoscrollpanel/NanoScrollPanel.java
@@ -2,8 +2,10 @@ package org.vaadin.hhe.nanoscrollpanel;
 
 import org.vaadin.hhe.nanoscrollpanel.gwt.client.shared.NanoScrollPanelState;
 
+import com.vaadin.annotations.JavaScript;
 import com.vaadin.ui.AbstractSingleComponentContainer;
 
+@JavaScript({"jquery.min.js","jquery.nanoscroller.min.js"})
 public class NanoScrollPanel extends AbstractSingleComponentContainer {
 
     private static final long serialVersionUID = -2168758555251706761L;
